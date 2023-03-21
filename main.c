97f99e70fc9626f9e042bc24f166fa4cd803458d
ayushsingh1610 <116502604+ayushsingh1610@users.noreply.github.com>
2023-03-21 14:37:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+//Print first 50 natural number 
+
 #include<stdio.h>
+int natural(int);
+void main()
+{
+    int n=1;
+    printf("The first 50 natural numbers are -");
+    natural(n);
+    
+}
+int natural(int a)
+{
+    if(a<=50)
+    {
+        printf("%d  ",a);
+        natural(a+1);
+    }
+    else
+        return 0;
+}
